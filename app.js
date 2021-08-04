b5a556b60f38957cec663a89ecdda9ef17e29d97
Viljoen <josh.viljoen@capgemini.com>
2021-08-04 18:26:15 +0100

    Add all my files

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Event = require('./models/event');
+var seedDB = require('./seeds');
+var Comment = require('./models/comment');
+var methodOverride = require('method-override');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var User = require('./models/user');
+
+
+var commentRoutes = require('./routes/comments');
+var eventRoutes = require('./routes/events');
+var indexRoutes = require('./routes/index');
+
+
+mongoose.connect('mongodb+srv://JGuest:JPassword@cluster0.zhszo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+
+
+app.use(require('express-session')({
+  secret: 'Passcode!!!',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
+
+app.use(indexRoutes);
+app.use(commentRoutes);
+app.use(eventRoutes);
+
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+  console.log("Server Started: port 3000");
+});
